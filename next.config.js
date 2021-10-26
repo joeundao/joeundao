582bf40bd426b510f76ab6859f0a854ff0137b6d
Anthony Lee <antwonlee@gmail.com>
2021-10-26 12:10:56 -0500

    feat: Install Hardhat with typescript (#2)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  experimental: {
+    urlImports: ["https://cdn.skypack.dev"],
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
