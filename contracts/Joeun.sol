9d4d6de645297e8e04dae939b32ab1a0c18ee3e9
Anthony Lee <antwonlee@gmail.com>
2021-11-07 18:19:32 -0600

    feat: Add JOEUN NFT (#3)

---
+++ contracts/Joeun.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract HFNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+  using Counters for Counters.Counter;
+
+  Counters.Counter private _tokenIdCounter;
+
+  constructor() ERC721("JOEUND NFT", "JOEUN") {}
+
+  function mintNFT(string memory nftURI) public returns (uint256) {
+    _tokenIdCounter.increment();
+    uint256 newItemId = _tokenIdCounter.current();
+
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, nftURI);
+
+    return newItemId;
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
+}
