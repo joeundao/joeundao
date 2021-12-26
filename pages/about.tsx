887933b8d3c4d37e3669dd1a6aa76f355f9e0d3d
Anthony Lee <anthonylee@Anthonys-MBP.attlocal.net>
2021-12-25 20:45:56 -0600

    wip: Adding about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <div className="md:container md:mx-auto">
+        <span className=" text-"></span>
+      </div>
+    </>
+  );
+};
+
+export default About;
