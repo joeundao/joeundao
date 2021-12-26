4a0bf283a5d652e79acd04c10f949827e4451115
Anthony Lee <anthonylee@Anthonys-MBP.attlocal.net>
2021-12-25 21:31:46 -0600

    wip: Adding about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,45 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import JoeunDAOLogo from "../public/joeundao-logo.png";
 // import Web3 from "web3";
 // import Web3Modal from "web3modal";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Joeun DAO</title>
         <meta name="description" content="Decentrailzed Community for Good." />
         <link rel="icon" href="/joeundao-logo.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
-      <main className={styles.main}>
-        <Image
-          src={JoeunDAOLogo}
-          width={200}
-          height={200}
-          alt="Joeun DAO Logo"
-        />
-        <span className="text-2xl">Joeun DAO (좋은다오)</span>
-        <small className="text-gray-500">
-          Decentralized Community for Good.
-        </small>
+      <main>
+        <div className="md:container md:mx-auto px-10">
+          <div className="mt-28">
+            <Image
+              src={JoeunDAOLogo}
+              width={100}
+              height={100}
+              placeholder="blur"
+              alt="Joeun DAO Logo"
+            />
+          </div>
+          <div className="text-2xl">Joeun DAO (좋은다오)</div>
+          <div>
+            <small className="text-gray-500">
+              Decentralized Community for Good.
+            </small>
+          </div>
+          <p className="text-5xl py-10 leading-relaxed text-gray-400">
+            Joeun DAO is a community of good humans trying to do random acts of
+            kindness worldwide. We believe kind acts have superpowers.
+          </p>
+        </div>
       </main>
     </div>
   );
