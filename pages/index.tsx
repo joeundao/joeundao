6f383ff848251f757e7abd5c1e12c31cf286c0d0
Anthony Lee <anthonylee@anthonys-mbp.attlocal.net>
2021-12-31 01:23:39 -0600

    feature: Add homepage marketing copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import JoeunDAOLogo from "../public/joeundao-logo.png";
 // import Web3 from "web3";
 // import Web3Modal from "web3modal";
+
+import { BeakerIcon } from "@heroicons/react/solid";
 
 const Home: NextPage = () => {
   return (
@@ -35,10 +38,53 @@ const Home: NextPage = () => {
               Decentralized Community for Good.
             </small>
           </div>
-          <p className="text-5xl py-10 leading-relaxed text-gray-400">
+          <p className="text-5xl py-10 leading-normal text-gray-400 w-3/4">
             Joeun DAO is a community of good humans trying to do random acts of
             kindness worldwide. We believe kind acts have superpowers.
           </p>
+          <div className="mt-20 w-2/3">
+            <span className="text-3xl">How DAO is different?</span>
+            <p className="mt-10 leading-relaxed text-gray-500">Simple.</p>
+            <p className="leading-relaxed text-gray-500">No middle man.</p>
+            <p className="leading-relaxed text-gray-500">
+              Donations go directly to recipient.
+            </p>
+          </div>
+          <div className="mt-20 w-2/3">
+            <span className="text-3xl">Why Joeun DAO?</span>
+            <p className="mt-10 leading-relaxed text-gray-500">
+              Decide who to give together.
+            </p>
+            <p className="leading-relaxed text-gray-500">
+              Two is better than one.
+            </p>
+            <p className="leading-relaxed text-gray-500">
+              Use open source and non-custodial liquidity pool for earning
+              interest.
+            </p>
+
+            <div className="mt-20">
+              <Link href="https://twitter.com/joeundao">
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 float-left"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                    />
+                  </svg>
+                  JoeunDAO
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
     </div>
