427e4b355cc0010df7b38b22d440f36c417cc012
Anthony Lee <anthonylee@anthonys-mbp.attlocal.net>
2022-01-08 03:11:23 -0600

    feat: Add basic wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,63 @@
 import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
+import {
+  InjectedConnector,
+  Provider,
+  WalletConnectConnector,
+  WalletLinkConnector,
+  chain,
+  defaultChains,
+} from "wagmi";
+
+const alchemyId = process.env.ALCHEMY_ID;
+const chains = defaultChains;
+
+const connectors = ({ chainId }) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0];
+  return [
+    new InjectedConnector({ chains }),
+    new WalletConnectConnector({
+      options: {
+        alchemyId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      options: {
+        appName: "Joeun DAO",
+        jsonRpcUrl: `${rpcUrl}/${alchemyId}`,
+      },
+    }),
+  ];
+};
 
 export default function JoeunApp({ Component, pageProps }) {
   return (
-    <SessionProvider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      options={{
-        // Stale Time controls how often the useSession in the client should
-        // contact the server to sync the session state. Value in seconds.
-        // e.g.
-        // * 0  - Disabled (always use cache value)
-        // * 60 - Sync session state with server if it's older than 60 seconds
-        staleTime: 0,
-        // Refetch Interval tells windows / tabs that are signed in to keep sending
-        // a keep alive request (which extends the current session expiry) to
-        // prevent sessions in open windows from expiring. Value in seconds.
-        //
-        // Note: If a session has expired when keep alive is triggered, all open
-        // windows / tabs will be updated to reflect the user is signed out.
-        refetchInterval: 0,
-      }}
-      session={pageProps.session}
-    >
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider autoConnect connectors={connectors}>
+      <SessionProvider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        options={{
+          // Stale Time controls how often the useSession in the client should
+          // contact the server to sync the session state. Value in seconds.
+          // e.g.
+          // * 0  - Disabled (always use cache value)
+          // * 60 - Sync session state with server if it's older than 60 seconds
+          staleTime: 0,
+          // Refetch Interval tells windows / tabs that are signed in to keep sending
+          // a keep alive request (which extends the current session expiry) to
+          // prevent sessions in open windows from expiring. Value in seconds.
+          //
+          // Note: If a session has expired when keep alive is triggered, all open
+          // windows / tabs will be updated to reflect the user is signed out.
+          refetchInterval: 0,
+        }}
+        session={pageProps.session}
+      >
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   );
 }
