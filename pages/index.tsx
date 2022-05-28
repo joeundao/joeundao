2fb5e0e9e491f2ccb0872ca7d7e37b6b35585b90
Anthony Lee <antwonlee@gmail.com>
2022-05-27 22:52:55 -0500

    chore: Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,25 +20,26 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <div className="px-10 mb-20 md:container md:mx-auto">
-          <div className="mt-10">
-            <Image
-              src={JoeunDAOLogo}
-              width={100}
-              height={100}
-              placeholder="blur"
-              alt="Joeun DAO Logo"
-            />
-          </div>
-          <div className="text-2xl">
-            Joeun DAO (좋은다오) <span className="text-xs align-top">Beta</span>
-          </div>
-          <div>
-            <small className="text-gray-500">
-              Decentralized Community for Good.
-            </small>
-          </div>
-          <div className="p-10 mt-10 bg-gray-100 rounded-lg lg:p-20 p-lg-20">
-            <p className="w-3/4 mb-10 text-2xl leading-relaxed text-gray-400 sm:text-4xl sm:leading-relaxed">
+          <div className="p-10 mt-10 border border-gray-200 rounded-lg shadow lg:p-20">
+            <div>
+              <Image
+                src={JoeunDAOLogo}
+                width={100}
+                height={100}
+                placeholder="blur"
+                alt="Joeun DAO Logo"
+              />
+            </div>
+            <div className="text-2xl">
+              Joeun DAO (좋은다오){" "}
+              <span className="text-xs align-top">Beta</span>
+            </div>
+            <div className="mb-10">
+              <small className="text-gray-500">
+                Decentralized Community for Good.
+              </small>
+            </div>
+            <p className="w-3/4 text-xl leading-relaxed text-gray-500 text-teal-800 sm:leading-relaxed">
               Joeun DAO is a community of good humans trying to do random acts
               of kindness worldwide. We believe kind acts have superpowers.
             </p>
